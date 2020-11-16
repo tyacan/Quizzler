1fe309bb00950fdfc45e1e0b2c4284966ec4e7d5
Muathye <muath.ye@gmail.com>
2020-11-16 08:23:44 +0300

    add question class and use it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -32,12 +33,28 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
+          flex: 1,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Question Number: $questionNumber',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white10,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,7 +81,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
                 setState(() {
                   questionNumber++;
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = questions[questionNumber].questionAnswer;
                   correctAnswer == true
                       ? scoreKeeper.add(Icon(
                           Icons.check,
@@ -95,7 +112,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 setState(() {
                   questionNumber++;
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = questions[questionNumber].questionAnswer;
                   correctAnswer == false
                       ? scoreKeeper.add(Icon(
                           Icons.check,
@@ -119,29 +136,9 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   List<Icon> scoreKeeper = [
-    // Ex: 
+    // Ex:
     // Icon(Icons.check, color: Colors.green),
     // Icon(Icons.close, color: Colors.red),
-  ];
-
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-    'A slug\'s blood is green.1',
-    'A slug\'s blood is green.2',
-    'A slug\'s blood is green.3',
-    'A slug\'s blood is green.4',
-    'A slug\'s blood is green.5',
-  ];
-
-  List<bool> answers = [
-    false,
-    false,
-    false,
-    true,
-    true,
-    true,
   ];
 
   // What is the current number of question
